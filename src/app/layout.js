8f00bdc6b8d7612d1d8c13c540721c1c14717cf4
jamal-uddin-asif <asifzehendmg@gmail.com>
2025-11-27 01:07:57 +0600

    set default theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme=''>
+    <html lang="en" >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
